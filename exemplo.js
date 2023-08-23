493fdfe9644ca23f38af159132a4ff4bece9d319
Anne-Ribeiro <84384217+Anne-Ribeiro@users.noreply.github.com>
2023-08-23 13:00:05 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,15 @@
+const {createServer, request} = require('http')
+
+let server = createServer((request, response)=>{
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write(`
+    <h1>HelloWord em Node</h1>
+    <p>Pagina de resposta do servidor HTTP</p>
+    `)
+    response.end();
+})
+
+const port = 8000
+server.listen(port, ()=>{
+    console.log(`Servidor http configurando na ${port} na url http://localhost:${port}`)
+});
